993f34cb50e4051f97c9144bd89db19a2d6a1ad9
Shang Jian Ding <sding3@ncsu.edu>
2020-02-28 21:59:58 -0600

    Add semver & move flag decl to init func

--- main.go
+++ main.go
@@ -4,30 +4,49 @@ import (
 	"flag"
 	"fmt"
 	"os"
+	"strings"
 	"time"
 )
 
+const (
+	versionMajor = 0
+	versionMinor = 0
+	versionPatch = 1
+)
+
+var CommandWatcher = &watcher{}
+
+func init() {
+	flag.ErrHelp = fmt.Errorf("flag error")
+	flag.StringVar(&CommandWatcher.command, "c", "", "[required] shell command")
+	flag.DurationVar(&CommandWatcher.interval, "n", time.Second, "update interval")
+	flag.BoolVar(&CommandWatcher.exitOnErr, "e", false, "exit on command error (default false)")
+}
+
 func main() {
-	w, err := NewWatcherFromFlag()
-	if err != nil {
+	flag.Parse()
+
+	if strings.ToLower(flag.Arg(0)) == "version" {
+		printVersion()
+		os.Exit(0)
+	}
+
+	if err := validateWatcher(CommandWatcher); err != nil {
 		fmt.Println(err)
 		flag.Usage()
 		os.Exit(1)
 	}
 
-	os.Exit(w.Start())
+	os.Exit(CommandWatcher.Start())
 }
 
-func NewWatcherFromFlag() (*watcher, error) {
-	w := &watcher{}
+func printVersion() {
+	fmt.Printf("taildiff version %d.%d.%d\n", versionMajor, versionMinor, versionPatch)
+}
 
-	flag.StringVar(&w.command, "c", "", "[required] shell command")
-	flag.DurationVar(&w.interval, "n", time.Second, "update interval")
-	flag.BoolVar(&w.exitOnErr, "e", false, "exit on command error (default false)")
-	flag.Parse()
+func validateWatcher(w *watcher) error {
 	if w.command == "" {
-		return nil, fmt.Errorf("A command is required. Supply command with -c <command>")
+		return fmt.Errorf("A command is required. Supply command with -c <command>")
 	}
-
-	return w, nil
+	return nil
 }
